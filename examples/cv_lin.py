e03252a83793a7a92c804b8079db489476b4878d
antcc <antoniocoin07@gmail.com>
2022-02-09 19:41:50 +0100

    Lowercase in check"

--- examples/cv_lin.py
+++ examples/cv_lin.py
@@ -70,7 +70,7 @@ pd.set_option('display.max_columns', 80)
 alg = sys.argv[1].lower()
 if ":" in alg:
     MCMC_ALG = alg.split(":")[0]
-    USE_NUTS = alg.split(":")[1] == "NUTS"
+    USE_NUTS = alg.split(":")[1] == "nuts"
 else:
     MCMC_ALG = alg
     USE_NUTS = False
